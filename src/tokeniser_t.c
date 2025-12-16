dcac02cf26cffd03062899c943de0b4cff021631
vs-123 <19247154+vs-123@users.noreply.github.com>
2025-12-16 04:15:58 +0000

    fixed .gitignore not tracking properly; added tokeniser_t.h/.c

---
+++ src/tokeniser_t.c
@@ -0,0 +1,339 @@
+#include "tokeniser_t.h"
+
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "str.h"
+
+line_col_t
+get_line_col (const tokeniser_t *tokeniser)
+{
+   const unsigned int idx = tokeniser->prog_idx;
+   const char *prog       = tokeniser->program;
+   unsigned int line      = 1;
+   unsigned int col       = 1;
+
+   char c = '\1';
+
+   for (unsigned int i = 0; i < idx; i++)
+      {
+         c = prog[i];
+         if (c == '\n')
+            {
+               line++;
+               col = 0;
+            }
+         col++;
+      }
+
+   line_col_t lc = {
+      .line = line,
+      .col  = col,
+   };
+
+   return lc;
+}
+
+// tbh compilers yelling errors
+// feels like it's crying
+// and yup this throws an error
+static void
+cry (const tokeniser_t *tokeniser, const char *format, ...)
+{
+   line_col_t lc = get_line_col (tokeniser);
+
+   fprintf (stderr, "[ERROR %u:%u] ", lc.line, lc.col);
+
+   va_list args;
+
+   va_start (args, format);
+   vfprintf (stderr, format, args);
+   va_end (args);
+
+   fprintf (stderr, "\n");
+
+   exit (1);
+}
+
+void
+tokenise_ident (tokeniser_t *tokeniser)
+{
+   // we're at an isalpha character right now
+   // eat up isalnum characters until we hit a non-isalnum char
+   // and then push it as a token to tokeniser->tokens
+
+   // starting with 32 capacity
+   string_t eaten_alnum = new_string (32);
+   char curr_char       = '\1';
+   line_col_t lc        = get_line_col (tokeniser);
+
+   while ((isalnum ((curr_char = tokeniser->program[tokeniser->prog_idx++]))
+           || curr_char == '_'))
+      {
+         string_push (&eaten_alnum, curr_char);
+      }
+
+   tokeniser->prog_idx--;
+
+   token_t *token = malloc (sizeof (token_t));
+   token->type    = Token_Ident;
+   token->c_val   = eaten_alnum.elems;
+   token->lc      = lc;
+
+   vector_push_elem (&tokeniser->tokens, (void *)token);
+}
+
+void
+tokenise_num (tokeniser_t *tokeniser)
+{
+   // we're at an isdigit character right now
+   // eat up isalnum characters until we hit a non-isdigit char
+   // and then push it as a token to tokeniser->tokens
+
+   // no malloc; it's a number
+   unsigned int eaten_num = 0;
+   char curr_char         = '\1';
+   line_col_t lc          = get_line_col (tokeniser);
+
+   while (isdigit ((curr_char = tokeniser->program[tokeniser->prog_idx++])))
+      {
+         eaten_num *= 10;
+         eaten_num += (curr_char - '0');
+      }
+
+   tokeniser->prog_idx--;
+
+   token_t *token = malloc (sizeof (token_t));
+   token->type    = Token_Number;
+   token->n_val   = eaten_num;
+   token->lc      = lc;
+
+   vector_push_elem (&tokeniser->tokens, (void *)token);
+}
+
+char *
+tok_to_str (token_type_t tok_type)
+{
+   switch (tok_type)
+      {
+         // MBF
+      case Token_Ident:
+         return "Token_Ident";
+      case Token_Number:
+         return "Token_Number";
+
+      case Token_LCurly:
+         return "Token_LCurly";
+      case Token_RCurly:
+         return "Token_RCurly";
+
+      case Token_Semicolon:
+         return "Token_Semicolon";
+
+         // Classic BF
+      case Token_Plus:
+         return "Token_Plus";
+      case Token_Minus:
+         return "Token_Minus";
+
+      case Token_Left:
+         return "Token_Left";
+      case Token_Right:
+         return "Token_Right";
+
+      case Token_LLoop:
+         return "Token_LLoop";
+      case Token_RLoop:
+         return "Token_RLoop";
+
+      case Token_Dot:
+         return "Token_Dot";
+
+         // Misc.
+      case Token_EOF:
+         return "Token_EOF";
+      }
+}
+
+void
+print_tokens (vector_t tokens)
+{
+   puts ("Printing Tokens");
+   puts ("===============");
+   for (unsigned int i = 0; i < tokens.size; i++)
+      {
+         token_t *curr_tok = (token_t *)vector_at (&tokens, i);
+	 token_type_t tt_t = curr_tok->type;
+         const char *tt    = tok_to_str (tt_t);
+
+         printf ("i: %d; ", i);
+         printf ("type: %s; ", tt);
+
+         if (tt_t == Token_Number)
+            {
+	       printf("n_val: %d;", curr_tok->n_val);
+            }
+         else if (tt_t == Token_Ident)
+            {
+               printf ("c_val: %s;", curr_tok->c_val);
+            }
+
+	 printf("\n");
+      }
+}
+
+void
+mbf_tokenise (tokeniser_t *tokeniser)
+{
+   tokeniser->tokens = new_vector (32, sizeof (token_t));
+   char current_char = '\1';
+
+   unsigned int prog_size = strlen (tokeniser->program);
+
+   while (tokeniser->prog_idx < prog_size)
+      {
+         current_char = tokeniser->program[tokeniser->prog_idx];
+         switch (current_char)
+            {
+            // comments
+            case '#':
+               while (current_char != '\n' && current_char != '\0')
+                  {
+                     current_char = tokeniser->program[++tokeniser->prog_idx];
+                  }
+               break;
+
+            // skip whitespaces, tabs and newlines
+            case '\t':
+            case '\n':
+            case '\r':
+            case ' ':
+               break;
+
+            case '{':
+               {
+                  token_t *token = malloc (sizeof (token_t));
+                  token->type    = Token_LCurly;
+                  token->lc      = get_line_col (tokeniser);
+                  vector_push_elem (&tokeniser->tokens, (void *)token);
+               }
+               break;
+
+            case '}':
+               {
+                  token_t *token = malloc (sizeof (token_t));
+                  token->type    = Token_RCurly;
+                  token->lc      = get_line_col (tokeniser);
+                  vector_push_elem (&tokeniser->tokens, (void *)token);
+               }
+               break;
+
+            case '+':
+               {
+                  token_t *token = malloc (sizeof (token_t));
+                  token->type    = Token_Plus;
+                  token->lc      = get_line_col (tokeniser);
+                  vector_push_elem (&tokeniser->tokens, (void *)token);
+               }
+               break;
+
+            case '-':
+               {
+                  token_t *token = malloc (sizeof (token_t));
+                  token->type    = Token_Minus;
+                  token->lc      = get_line_col (tokeniser);
+                  vector_push_elem (&tokeniser->tokens, (void *)token);
+               }
+               break;
+
+            case '<':
+               {
+                  token_t *token = malloc (sizeof (token_t));
+                  token->type    = Token_Left;
+                  token->lc      = get_line_col (tokeniser);
+                  vector_push_elem (&tokeniser->tokens, (void *)token);
+               }
+               break;
+
+            case '>':
+               {
+                  token_t *token = malloc (sizeof (token_t));
+                  token->type    = Token_Right;
+                  token->lc      = get_line_col (tokeniser);
+                  vector_push_elem (&tokeniser->tokens, (void *)token);
+               }
+               break;
+
+            case '[':
+               {
+                  token_t *token = malloc (sizeof (token_t));
+                  token->type    = Token_LLoop;
+                  token->lc      = get_line_col (tokeniser);
+                  vector_push_elem (&tokeniser->tokens, (void *)token);
+               }
+               break;
+
+            case ']':
+               {
+                  token_t *token = malloc (sizeof (token_t));
+                  token->type    = Token_RLoop;
+                  token->lc      = get_line_col (tokeniser);
+                  vector_push_elem (&tokeniser->tokens, (void *)token);
+               }
+               break;
+
+            case '.':
+               {
+                  token_t *token = malloc (sizeof (token_t));
+                  token->type    = Token_Dot;
+                  token->lc      = get_line_col (tokeniser);
+                  vector_push_elem (&tokeniser->tokens, (void *)token);
+               }
+               break;
+
+            case ';':
+               {
+                  token_t *token = malloc (sizeof (token_t));
+                  token->type    = Token_Semicolon;
+                  token->lc      = get_line_col (tokeniser);
+                  vector_push_elem (&tokeniser->tokens, (void *)token);
+               }
+               break;
+
+            case '\0':
+               {
+                  token_t *token = malloc (sizeof (token_t));
+                  token->type    = Token_EOF;
+                  token->lc      = get_line_col (tokeniser);
+                  vector_push_elem (&tokeniser->tokens, (void *)token);
+               }
+               break;
+
+            default:
+               if (isalpha (current_char))
+                  {
+                     // cry(tokeniser, "before curr char : %c", current_char);
+
+                     tokenise_ident (tokeniser);
+                     // cry(tokeniser, "after curr char : %c", current_char);
+                     // const token_t *ident = vector_at (&tokeniser->tokens,
+                     // 0); cry (tokeniser, "found ident: %s", ident->c_val);
+                  }
+               else if (isdigit (current_char))
+                  {
+                     tokenise_num (tokeniser);
+		     continue;
+                  }
+               else
+                  {
+                     cry (tokeniser, "found weird char `%c` (code: %d)\n",
+                          current_char, (int)current_char);
+                  }
+            }
+
+         tokeniser->prog_idx++;
+      }
+}
---
+++ src/tokeniser_t.h
@@ -0,0 +1,61 @@
+#ifndef TOKENISER_T_H
+#define TOKENISER_T_H
+
+#include "vector.h"
+
+typedef struct
+{
+   unsigned int line;
+   unsigned int col;
+} line_col_t;
+
+typedef enum
+{
+   // MBF
+   Token_Ident,
+   Token_Number,
+
+   Token_LCurly,
+   Token_RCurly,
+
+   Token_Semicolon,
+
+   // Classic BF
+   Token_Plus,
+   Token_Minus,
+
+   Token_Left,
+   Token_Right,
+
+   Token_LLoop,
+   Token_RLoop,
+
+   Token_Dot,
+
+   // Misc.
+   Token_EOF,
+} token_type_t;
+
+typedef struct
+{
+   token_type_t type;
+   union
+   {
+      unsigned int n_val;
+      const char *c_val;
+   };
+   line_col_t lc; // line & col
+} token_t;
+
+typedef struct
+{
+   const char *program;
+   unsigned int prog_idx;
+   vector_t tokens; // vector_t of token_t's
+} tokeniser_t;
+
+char *tok_to_str (token_type_t tok);
+void print_tokens(vector_t tokens);
+void mbf_tokenise (tokeniser_t *tokeniser);
+
+#endif // TOKENISER_T_H
